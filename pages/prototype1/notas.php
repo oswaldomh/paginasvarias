1d241ecf7895bda616f27d43edd4c25d3d353588
oswaldomh <oswaldo.maldonado.888@gmail.com>
2022-12-29 13:35:25 -0600

    Agregué el prototipo de la pagina de la cafeteria con bootstrap 3

---
+++ pages/prototype1/notas.php
@@ -0,0 +1,98 @@
+$juego = array
+    (
+    array("uno",rand(1,6)),
+    array("dos",rand(1,6)),
+    array("tres",rand(1,6)),
+    array("cuatro",rand(1,6),)
+    array("cinco",rand(1,6)),
+    array("seis",rand(1,6)),
+    );
+    $array = ['uno','dos','tres','cuatro','cinco','seis'];
+
+    echo $array[0];
+    echo "<br>";
+    echo $array[1];
+    echo "<br>";
+    echo $array[2];
+    echo "<br>";
+    echo $array[3];
+
+    echo(rand(1,6)) . "<br>";
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    $txd = $tiros*$dados
+
+
+
+
+
+
+    $dados = array
+	(
+ 	array("uno",$uno),
+    array("dos",$dos),
+    array("tres",$tres),
+    array("cuatro",$cuatro),
+    array("cinco",$cinco),
+    array("seis",$seis),
+    );
+    echo $dados[0][0].": Randomnum: ".$uno.".<br>";
+	echo $dados[1][0].": Randomnum: ".$dos.".<br>";
+	echo $dados[2][0].": Randomnum: ".$tres.".<br>";
+    echo $dados[3][0].": Randomnum: ".$cuatro.".<br>";
+	echo $dados[4][0].": Randomnum: ".$cinco.".<br>";
+	echo $dados[5][0].": Randomnum: ".$seis.".<br>";
+    
+
+
+
+
+
+    if ($txd >= 100){
+        echo "Son muchos datos, no puedo procesar tantos :(";
+    }
+else{
+    for($i = 0; $i < $txd; ++$i) {
+    $variablex[i] = (rand(1,6));      
+    }
+    echo "dado"[i]=$variablex[i]
+    }
+
+
+
+
+
+
+    if ($favorito === ){
+        echo "ganaste";
+    }
+    else {echo "perdiste";
+    }?><br>
+
+
+
+
+$txd =     
+for($i = 0; $i < $txd; ++$i) {
+   $uno ($txd)= (rand(1,6)); 
+}
